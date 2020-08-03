ff6f5be4c09eb1be2b015b4d52a8b80b21d7694e
Darrell S. Powe III <44445781+dpowe3rd@users.noreply.github.com>
2020-08-02 20:05:35 -0400

    Additional Testing Files were added, Scraping for kills and matches played has been added.

--- MW_Script.py
+++ MW_Script.py
@@ -4,21 +4,43 @@ import selenium
 import json
 import pprint
 
-pp = pprint.PrettyPrinter(width=4000)
+pp = pprint.PrettyPrinter(width=4000)  # Pretty Print to make Console text Easy to read
 
 for num in range(23309):
-    num = num + 1
+    num += 1  # Incrementing
+
     url = 'https://cod.tracker.gg/warzone/leaderboards/stats/all/Kills?page={}'.format(num)
-    res = requests.get(url)
+    res = requests.get(url)  # Using Requests to get a response model
     bs = BeautifulSoup(res.text, 'lxml')
-    t_body = bs.find_all(class_='username')
-    p = []
 
-    for x in t_body:
-        p.append(str(x))
+    url2 = 'https://cod.tracker.gg/warzone/leaderboards/stats/all/Kills?page=1'
+    res2 = requests.get(url2)
+    html_soup = BeautifulSoup(res2.text, 'html.parser')
 
-    for j in range(len(p)):
-        user = [str(p[j][143:]).split('>')[0]]
+    users = bs.find_all(class_='username')
+    stats = html_soup.find_all('td', class_='stat')
 
-        print(user)
+    players = []
+    use = []
+    temp = []
+    k = 2 in range(len(stats))
 
+    for x in users:
+        players.append(str(x))
+
+        for j in range(len(players)):
+            user = [str(players[j][160:]).split('>')[0]]
+
+            for i in range(len(stats)):
+                kills = str(stats[i])[50:].split('>')[1].split('<')[0]
+                matches = str(stats[i:k])[50:].split('>')[1].split('<')[0]
+
+                if kills > matches:                 # TODO Fix list out of bounds error
+                    # print([kills, matches])
+                    temp[i].append(kills, matches)
+                else:
+                    pass
+
+        print(user[0].split('/') + temp)
+
+    # use.append(user[0].split('/'))
--- Testing_for.py
+++ Testing_for.py
@@ -4,21 +4,23 @@ import pprint
 
 pp = pprint.PrettyPrinter(width=4000)
 
-for num in range(23309):
-    num += 1
-    url = 'https://cod.tracker.gg/warzone/leaderboards/stats/all/Kills?page={}'.format(num)
-    res = requests.get(url)
-    bs = BeautifulSoup(res.text, 'lxml')
-    t_body = bs.find_all(class_='username')
-    p = []
+# for num in range(23309):
+#     num += 1
+url = 'https://cod.tracker.gg/warzone/leaderboards/stats/all/Kills?page=1'
+res = requests.get(url)
+bs = BeautifulSoup(res.text, 'lxml')
+j_body = bs.find_all(class_='username')
+p = []
 
-    for x in t_body:
-        p.append(str(x))
+for x in j_body:
+    user = []
+    p.append(str(x))
 
     for j in range(len(p)):
-        user = [str(p[j][143:]).split('>')[0]]
+        user.append(str(p[j][143:]).split('>'))
+        # [str(p[j][143:]).split('>')[0]]
 
-        print(user)
+    print(user)
 
 
 # for tr in t_body:
